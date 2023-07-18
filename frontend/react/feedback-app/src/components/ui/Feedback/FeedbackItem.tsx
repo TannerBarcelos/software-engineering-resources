131464c15ee07b593064db77fa18edb8d9931b1e
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2023-07-17 17:02:38 -0700

    Adding my own feat: voting

--- frontend/react/feedback-app/src/components/ui/Feedback/FeedbackItem.tsx
+++ frontend/react/feedback-app/src/components/ui/Feedback/FeedbackItem.tsx
@@ -1,5 +1,6 @@
 import Card from '../../shared/Card'
 import { feedbackItem } from './types'
+import { FaThumbsUp, FaThumbsDown, FaTimes } from 'react-icons/fa'
 
 type FeedbackItemProps = {
     item: feedbackItem
@@ -7,11 +8,17 @@ type FeedbackItemProps = {
 
 const FeedbackItem = ({ item }: FeedbackItemProps) => {
     return (
-        // Universal card component that can be used for any type of card and takes in children as a prop
         <Card invert={ true }>
             <div className="num-display">{ item.rating }</div>
+            <button className="close">
+                <FaTimes color='purple' />
+            </button>
             <div className="text-display">
                 { item.description }
+            </div>
+            <div className="reactions">
+                <p>{ item.upvotes } <FaThumbsUp /></p>
+                <p>{ item.downvotes } <FaThumbsDown /></p>
             </div>
         </Card>
     )
