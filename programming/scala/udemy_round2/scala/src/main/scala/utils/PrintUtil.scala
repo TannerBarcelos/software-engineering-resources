4c7a55c51e4ff130af258385d0d11ccb09af8952
Tanner Barcelos <41309200+TannerBarcelos@users.noreply.github.com>
2023-08-09 18:53:48 -0700

    Update PrintUtil.scala

--- programming/scala/udemy_round2/scala/src/main/scala/utils/PrintUtil.scala
+++ programming/scala/udemy_round2/scala/src/main/scala/utils/PrintUtil.scala
@@ -1,14 +1,12 @@
 package utils
 
-import java.util
-
 object PrintUtil {
 
   private def printer[T](printable: T): Unit = {
     println(printable)
   }
 
-  def listPrinter[T](list: util.ArrayList[T]): Unit = {
+  def listPrinter[T](list: java.util.ArrayList[T]): Unit = {
     list.forEach(printer)
   }
 }
