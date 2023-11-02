cd28dae18f6fa992ad0373e0ecf41a9d078179f6
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2023-11-02 14:55:08 -0700

    Working with types and functions

--- programming/typescript/ts-basics/app.ts
+++ programming/typescript/ts-basics/app.ts
@@ -1,3 +1,21 @@
-// tsc -w app.ts can be used to watch for changes in the file and compile it automatically
+// Core Types
+// string, number, boolean
 
-console.log('Hello world!!!')
+let userName: string = 'Tanner'
+let userAge: number = 28
+let userIsAdult: boolean = userAge > 18
+
+function isAdult(age: number) {
+  return age > 18
+}
+
+function printUser(name: string, age: number) {
+  const adult = isAdult(age)
+  console.log(
+    `The users name is ${name} and their age is ${age} which means they ${
+      adult ? 'are' : 'are not'
+    } an adult.`,
+  )
+}
+
+printUser(userName, userAge)
