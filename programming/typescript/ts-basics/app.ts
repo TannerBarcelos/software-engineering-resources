e6a8e815a163972a907bd6247de304d806703790
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2023-11-13 15:11:22 -0800

    Objects and arrays

--- programming/typescript/ts-basics/app.ts
+++ programming/typescript/ts-basics/app.ts
@@ -1,21 +1,46 @@
+/**
+ * Compile with the following command:
+ * tsc -w app.ts --outDir dist
+ */
+
 // Core Types
 // string, number, boolean
 
 let userName: string = 'Tanner'
-let userAge: number = 28
-let userIsAdult: boolean = userAge > 18
+let userAge: number = 27
+let userIsAdult: boolean = userAge > 18 // true
 
-function isAdult(age: number) {
-  return age > 18
+// Object Types
+// object, Array, Tuple, Enum, Any
+
+type User = {
+  name: string
+  age: number
+  interests: string[]
 }
 
-function printUser(name: string, age: number) {
-  const adult = isAdult(age)
+// user is an object type which is of type User
+const user: User = {
+  name: userName,
+  age: userAge,
+  interests: ['Golf', 'Coding', 'Academia'],
+}
+
+function printUser(user: User) {
   console.log(
-    `The users name is ${name} and their age is ${age} which means they ${
-      adult ? 'are' : 'are not'
-    } an adult.`,
+    `The users name is ${user.name} and their age is ${
+      user.age
+    } which means they ${user.age > 18 ? 'are' : 'are not'} an adult.`,
   )
+  if (user.interests?.length) {
+    for (const interest of user.interests) {
+      console.log(`${user.name} is interested in ${interest}`)
+    }
+  } else {
+    console.log(`${user.name} has no interests.`)
+  }
 }
 
-printUser(userName, userAge)
+printUser(user)
+
+// console.log(Object.entries(user)) // [['name', 'Tanner'], ['age', 27], ['interests', ['Golf', 'Coding', 'Academia']]]
