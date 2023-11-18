9aabbf40eb789d4186bea2979c7f407fca39068a
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2023-11-18 15:54:51 -0800

    Learning folder structure by changing some variables / values

--- software-development/web-development/backend-development/backend-frameworks/typescript/nestjs/learning-nestjs/src/app.controller.ts
+++ software-development/web-development/backend-development/backend-frameworks/typescript/nestjs/learning-nestjs/src/app.controller.ts
@@ -3,9 +3,11 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  // The constructor is where we inject dependencies.
+  // In this case, we inject the AppService as a constructor parameter dependency.(Injected by provider)
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/')
   getHello(): string {
     return this.appService.getHello();
   }
