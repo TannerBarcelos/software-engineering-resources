97efcd9330f1c349c40f6bb2e0c7603f8b0955ca
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-01-23 11:33:36 -0800

    learning file structure

--- software-development/web-development/frontend-development/js-frameworks/angular/appointment-app/src/app/app.component.ts
+++ software-development/web-development/frontend-development/js-frameworks/angular/appointment-app/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', // select the <app-root> tag in index.html to render this component similar to <div id="root"></div> and .render() in React
+  templateUrl: './app.component.html', // the HTML template to render when this component is used
+  styleUrls: ['./app.component.css'], // the CSS styles to apply to this component
 })
 export class AppComponent {
   title = 'appointment-app';
