ac699215c0c21224190caba9aebb4659f5b410b2
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-01-23 20:52:24 -0800

    Update dependencies and add Bootstrap

--- software-development/web-development/frontend-development/js-frameworks/angular/hotel-app/src/app/home/home.module.ts
+++ software-development/web-development/frontend-development/js-frameworks/angular/hotel-app/src/app/home/home.module.ts
@@ -6,5 +6,6 @@ import { RouterModule } from '@angular/router'; // enables routerLink and router
 @NgModule({
   declarations: [HomeComponent],
   imports: [CommonModule, RouterModule],
+  exports: [HomeComponent], // other modules can use the components in this module now by exporting them here and importing this module in any other module that needs to use the components in this module
 })
 export class HomeModule {}
