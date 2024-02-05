d7895324642237ea385f9088dc95d1c5acc49967
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-02-05 14:05:58 -0800

    GrideR

---
+++ software-development/programming/go/go-grider/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+// | Command      | Explanation                                                  |
+// |--------------|--------------------------------------------------------------|
+// | `go build`   | **Compiles** the Go source code files in the project (./main) will run the file        |
+// | `go run`     | **Compiles and executes** the executable from the compile step. |
