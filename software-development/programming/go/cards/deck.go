c364dd8cbffb29553c8992f06822c830fdc54efe
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-02-05 17:11:54 -0800

    Learning more

--- software-development/programming/go/cards/deck.go
+++ software-development/programming/go/cards/deck.go
@@ -1,27 +1,24 @@
 package main
 
-// Deck is a string slice which represents a deck of playing cards where each card is a string value
 type Deck []string
 
-// This function is a receiver function for the Deck type
-// A receiver function is a function that is attached to a type which enables us to call the function on an instance of the type
-// In this case, we can call the Print function on an instance of the Deck type to print the deck of cards
-// Anything with type Deck can call this function
-// Remember: OOP doesn't exist in Go, but this is the closest thing to it
+// Shows all cards in the deck
 func (d Deck) print() {
 	for _, card := range d {
 		println(card)
 	}
 }
 
-func (d Deck) generateCard() string {
-	card := "Five of Diamonds"
-	return card
-}
+// Generates a fresh deck of cards to play with
+func newDeck() Deck {
+	deck := Deck{}
+	suits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	values := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Jack", "Queen", "King"}
 
-func (d Deck) generateDeck(deckSize int) Deck {
-	for i := 0; i < deckSize; i++ {
-		d = append(d, d.generateCard())
+	for _, suit := range suits {
+		for _, value := range values {
+			deck = append(deck, value+" of "+suit)
+		}
 	}
-	return d
+	return deck
 }
