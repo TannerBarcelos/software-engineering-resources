89f28ada1c87a64f64690234b410b97def98f128
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-02-05 17:28:11 -0800

    Callout

--- software-development/programming/go/cards/main.go
+++ software-development/programming/go/cards/main.go
@@ -2,7 +2,7 @@ package main
 
 func main() {
 	deck := newDeck()
-	hand, deck := deal(deck, 4)
+	hand, deck := deal(deck, 4) // will not modify the original deck since we did not use a pointer
 
 	hand.print() // again, can call print() since it is a receiver of the Deck type (acting as an instance)
 	deck.print()
