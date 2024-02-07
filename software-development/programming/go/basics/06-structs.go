856c8af954028ef2fc0246ab0165d33c393253c7
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-02-06 16:02:05 -0800

    More on structs

--- software-development/programming/go/basics/06-structs.go
+++ software-development/programming/go/basics/06-structs.go
@@ -3,36 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	// Idiomatic Go code uses CamelCase for Struct names
+	// Idiomatic Go code uses CamelCase for Struct names and fields
 	type Animal struct {
-		Name string
-		Age int
+		Name  string
+		Age   int
 		Color string
 	}
 
 	// Create a new Animal struct -- notice no space between the struct name and the curly brace
 	Willow := Animal{
-		Name: "Willow",
-		Age: 3,
+		Name:  "Willow",
+		Age:   3,
 		Color: "Black and White",
 	}
-	fmt.Println(Willow.Name, "is a", Willow.Color, "dog who is", Willow.Age, "years old.")
-
+	fmt.Printf("%+v\n", Willow) // %+v prints the field names along with the values
 
 	// Shorter way to create a new Animal struct
-	var Max = Animal{"Max", 5, "Brown"}
+	Max := Animal{"Max", 5, "Brown"}
 	fmt.Println(Max)
-	fmt.Println(Max.Name, "is a", Max.Color, "dog who is", Max.Age, "years old.") // Can still access fields by name even when using the shorter way to create a struct
-
+	fmt.Printf("%+v\n", Max)
 
 	// We can also create a struct and assign values to fields in one go
 	willow := struct {
-		Name string
-		Age int
+		Name  string
+		Age   int
 		Color string
-	} {
-		Name: "Willow",
-		Age: 3,
+	}{
+		Name:  "Willow",
+		Age:   3,
 		Color: "Black and White",
 	}
 
