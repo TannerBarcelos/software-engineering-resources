cb0a257f7b1c47a7ca1382a24dc7c491e25c686a
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-02-09 13:13:20 -0800

    Refactor code to use io.Copy for reading response body

--- software-development/programming/go/go-grider/projects/http/main.go
+++ software-development/programming/go/go-grider/projects/http/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
@@ -15,9 +16,12 @@ func main() {
 		os.Exit(1)
 	}
 
-	// This work is abstracted away when using a web framework like gin - the framework does this for you
-	bs := make([]byte, 99999)
-	resp.Body.Read(bs)
-	fmt.Println(string(bs))
+	// // This work is abstracted away when using a web framework like gin - the framework does this for you
+	// bs := make([]byte, 99999)
+	// resp.Body.Read(bs)
+	// fmt.Println(string(bs))
+
+	// Abstracting away the above code - using io.Copy.
+	io.Copy(os.Stdout, resp.Body)
 
 }
