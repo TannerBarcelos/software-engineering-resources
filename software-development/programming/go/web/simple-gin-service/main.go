cd125efae6810cf9cf44db7c97d965ee03a1fa58
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-02-09 15:30:46 -0800

    Init env loader and utils package

--- software-development/programming/go/web/simple-gin-service/main.go
+++ software-development/programming/go/web/simple-gin-service/main.go
@@ -1,13 +1,20 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/tannerbarcelos/simple-gin-service/utils"
+)
 
 func main() {
+	utils.LoadEnv()
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
-	r.Run(":3131")
+	r.Run(os.Getenv("PORT"))
 }
