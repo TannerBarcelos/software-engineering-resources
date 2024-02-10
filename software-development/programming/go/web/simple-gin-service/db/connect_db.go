12e16273c0e5724bafbf87ac85b9ee4458446de0
Tanner Barcelos <41309200+TannerBarcelos@users.noreply.github.com>
2024-02-09 22:26:46 -0800

    Update connect_db.go

--- software-development/programming/go/web/simple-gin-service/db/connect_db.go
+++ software-development/programming/go/web/simple-gin-service/db/connect_db.go
@@ -8,14 +8,23 @@ import (
 	"gorm.io/gorm"
 )
 
+var db *gorm.DB
+
 func ConnectDB() {
 	DB_NAME := os.Getenv("DB_NAME")
 
-	_, err := gorm.Open(sqlite.Open(DB_NAME), &gorm.Config{})
+    d, err := gorm.Open(sqlite.Open(DB_NAME), &gorm.Config{})
 
 	if err != nil {
 		panic("failed to connect database")
 	}
 
+    db = d // assign the pointer so we now have a variable of shared database logic for the whole app
+
 	log.Printf("Connected to %s", DB_NAME)
+}
+
+// returns the DB instance 
+func GetDB() *gorm.DB {
+  return db
 }
