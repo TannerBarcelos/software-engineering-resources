3690358b7e417e47c5d228df99168f7f5a14b90d
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-02-15 17:48:25 -0800

    Add concurrency using goroutines and channels

--- software-development/programming/go/go-grider/concurrency/channels/cmd/main/main.go
+++ software-development/programming/go/go-grider/concurrency/channels/cmd/main/main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
+	"time"
 )
 
 type Links []string
 
 func main() {
+
 	links := Links{
 		"http://www.google.com",
 		"http://www.facebook.com",
@@ -16,23 +17,38 @@ func main() {
 		"http://www.amazon.com",
 	}
 
+	// Create a channel to enable communication between go routines, specifically sharing strings between go routines
+	c := make(chan string)
+
 	for _, link := range links {
-		go checkLink(link) // "go" keyword creates a new go routine (A go routine is a lightweight thread of execution that is spawned by the main go routine and runs concurrently with the main go routine)
+		go checkLink(link, c) // "go" keyword creates a new go routine (A go routine is a lightweight thread of execution that is spawned by the main go routine and runs concurrently with the main go routine)
+	}
+
+	// fmt.Println(<-c) // This will block the main go routine until a value is received from the channel
+	// fmt.Println(<-c)
+	// fmt.Println(<-c)
+	// fmt.Println(<-c)
+	// fmt.Println(<-c) // we had 4 links but we are trying to read from the channel 5 times, this will cause the main go routine to block indefinitely. So, we know channels are bound by a size, and we can only read from them as many times as they have values. We can fix this by using a for loop to read from the channel as many times as there are links
+
+	// We can range over channels to read from them
+	for link := range c {
+		go checkLink(link, c) // infinite loop
+		// fmt.Println(link)
 	}
 }
 
-func checkLink(link string) {
-	response, err := http.Get(link)
+func checkLink(link string, c chan string) {
+	time.Sleep(5 * time.Second) // sleep for 5 seconds
+
+	_, err := http.Get(link)
 
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println(err)
+		c <- link + " is down"
 	}
 
-	if response.StatusCode != 200 {
-		log.Fatalf("Error: %s is down", link)
-	}
+	fmt.Println(link)
 
-	defer response.Body.Close()
-
-	fmt.Println(link, "is up")
+	// Send the link to the channel
+	c <- link
 }
