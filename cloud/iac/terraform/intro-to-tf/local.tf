01416d5c47685e01c0d12027d32d814796ac486c
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-02-27 15:20:54 -0800

    Update local.tf file to use tf-cool.txt instead of pet.txt

--- cloud/iac/terraform/intro-to-tf/local.tf
+++ cloud/iac/terraform/intro-to-tf/local.tf
@@ -1,5 +1,6 @@
+# block_type # block_resource_name_label # block_name_label
 resource "local_file" "pet" {
-  content  = "This is a pet"
-  filename = "pet.txt"
-  file_permission = "0700" # RWE for owner, nothing for group and others
+  content  = "Terraform is pretty cool!"    # Content of the file
+filename = "${path.module}/tf-cool.txt"     # Name of the file (this is a required field)
+  file_permission = "0700"                  # RWE for owner, nothing for group and others
 }
