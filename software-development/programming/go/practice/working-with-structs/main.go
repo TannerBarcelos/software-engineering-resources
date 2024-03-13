3a63ee4b246864fca6ed23b71aa5498cefc895c8
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-03-13 16:53:00 -0700

    Refactor code to use Users struct and add PrintUsers method

--- software-development/programming/go/practice/working-with-structs/main.go
+++ software-development/programming/go/practice/working-with-structs/main.go
@@ -1,18 +1,8 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	users := []User{}
-	i := 1
-	for i < 3 {
-		user := NewUser("Bob", "Doe", "test@test.com", i*10)
-		users = append(users, *user)
-		i++
-	}
-	for _, user := range users {
-		fmt.Println(user.GetUser())
-	}
+	users := Users{}
+	users.AddUser(NewUser("Bob", "Doe", "test@test.com", 23))
+	users.AddUser(NewUser("John", "Doe", "test2@gmail.com", 25))
+	users.PrintUsers()
 }
--- software-development/programming/go/practice/working-with-structs/user.go
+++ software-development/programming/go/practice/working-with-structs/user.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
+type Users struct {
+	Users []User
+}
+
+func (u *User) GetUser() string {
+	return fmt.Sprintf("%s %s, %s, %d", u.FirstName, u.LastName, u.Email, u.Age)
+}
+
+func (u *Users) AddUser(user *User) {
+	u.Users = append(u.Users, *user)
+}
+
+func (u *Users) GetUsers() *[]User {
+	return &u.Users
+}
+
+func (u *Users) PrintUsers() {
+	for _, user := range u.Users {
+		fmt.Println(user.GetUser())
+	}
+}
+
 type User struct {
 	FirstName string
 	LastName  string
@@ -16,8 +38,4 @@ func NewUser(firstName, lastName, email string, age int) *User {
 		Email:     email,
 		Age:       age,
 	}
-}
-
-func (u *User) GetUser() string {
-	return fmt.Sprintf("%s %s, %s, %d", u.FirstName, u.LastName, u.Email, u.Age)
 }
