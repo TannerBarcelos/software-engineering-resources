2a37847075061396972181e495fc669a07a8b470
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-03-28 18:08:50 -0700

    Clean

--- software-development/programming/go/topics/flag-library/main.go
+++ software-development/programming/go/topics/flag-library/main.go
@@ -5,8 +5,13 @@ import (
 	"fmt"
 )
 
+type ServerConfig struct {
+	port int
+}
+
 func main() {
-	age := flag.Int("age", 21, "age of the person")
+	var serverConfig ServerConfig
+	flag.IntVar(&serverConfig.port, "port", 8080, "Port to listen on")
 	flag.Parse()
-	fmt.Printf("Age: %d\n", *age)
+	fmt.Printf("Server will listen on port %d\n", serverConfig.port)
 }
