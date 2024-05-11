00d25da103c2a161bd777517fb8efeaf48ecde00
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-05-11 16:54:44 -0700

    Learning more SQL refreshers

--- databases/relational/mysql/core/creating_tables.sql
+++ databases/relational/mysql/core/creating_tables.sql
@@ -1,4 +1,5 @@
-# Creating a table - We create based off a data model (ERD) we can create on paper or an app like eraser.io CREATE TABLE IF NOT EXISTS tanners_sample_table (
+# Creating a table - We create based off a data model (ERD) we can create on paper or an app like eraser.io 
+CREATE TABLE IF NOT EXISTS tanners_sample_table (
 	user_id INT NOT NULL PRIMARY KEY,
 	first_name VARCHAR(50),
 	last_name VARCHAR(50),
@@ -7,6 +8,9 @@
 # $999.99/hr max
 hired_on_date DATE
 );
+
+# Describe the shape and properties of a table
+DESCRIBE tanners_sample_table;
 
 # Inserting records into a table
 INSERT INTO tanners_sample_table
@@ -28,7 +32,7 @@ SET
 WHERE
 	user_id = 1;
 
-# Update column name and optionally its data type
+# Change Column: allows changing the data type and properties but also allows renaming the column simultaneously.
 ALTER TABLE tanners_sample_table CHANGE COLUMN phone_no phone_number VARCHAR(12);
 
 SELECT
@@ -44,5 +48,18 @@ SELECT
 FROM
 	tanners_sample_table;
 
-# modify a column to change its properties but not its name.
-ALTER TABLE tanners_sample_table modify COLUMN phone_number VARCHAR(24);
+# Modify column: used to change the data type or other properties of an existing column but not its name
+ALTER TABLE tanners_sample_table MODIFY COLUMN phone_number VARCHAR(24);
+
+# multiple inserts in one transaction
+INSERT INTO tanners_sample_table
+	VALUES
+	(2, 'bob', 'builder', 22.00, '2021-01-02', '888-999-1111'), 
+	(3, 'snob', 'ilder', 25.00, '2022-01-02', '888-999-2222'), 
+	(4, 'cob', 'kilder', 44.00, '2023-01-02', '888-999-3333');
+
+SELECT
+	*
+FROM
+	tanners_sample_table;
+
