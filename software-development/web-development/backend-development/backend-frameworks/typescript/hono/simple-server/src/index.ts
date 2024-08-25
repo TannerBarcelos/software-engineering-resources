253ae1778a6964f1b8cb92ceece3698c7469cd70
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-08-25 16:24:05 -0700

    chore: Update Hono server response to return JSON message

--- software-development/web-development/backend-development/backend-frameworks/typescript/hono/simple-server/src/index.ts
+++ software-development/web-development/backend-development/backend-frameworks/typescript/hono/simple-server/src/index.ts
@@ -2,8 +2,10 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.get('/', (ctx) => {
+  return ctx.json({
+    message: "Hello, World!",
+  })
 })
 
 export default app
