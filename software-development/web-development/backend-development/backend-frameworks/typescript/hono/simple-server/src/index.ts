77394dc5569fc382028488deb623a38a69fe5cfb
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-08-25 16:51:58 -0700

    chore: Add streaming text route to server

--- software-development/web-development/backend-development/backend-frameworks/typescript/hono/simple-server/src/index.ts
+++ software-development/web-development/backend-development/backend-frameworks/typescript/hono/simple-server/src/index.ts
@@ -1,10 +1,37 @@
 import { Hono } from 'hono'
+import { streamText } from 'hono/streaming'
 
 const app = new Hono()
 
+// Simple route example.
 app.get('/', (ctx) => {
   return ctx.json({
     message: "Hello, World!",
+  })
+})
+
+const paragraph = `
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+  Nullam nec purus et eros facilisis tincidunt
+`
+
+const chunkWithOverlap = (text: string, chunkSize: number = 25, overlap: number = 3) => {
+  const chunks = []
+  for(let i = 0; i < text.length; i += chunkSize - overlap) {
+    chunks.push(text.slice(i, i + chunkSize))
+  }
+  return chunks
+}
+
+// Streaming text example.
+app.get('/streamText', (c) => {
+  const chunks = chunkWithOverlap(paragraph)
+  return streamText(c, async (stream) => {
+    for (const chunk of chunks) {
+      await stream.sleep(1000)
+      stream.write(chunk)
+    }
+    stream.close()
   })
 })
 
