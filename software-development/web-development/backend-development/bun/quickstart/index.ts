8a0f8fb90b92572a70babcc767378bf284ded0c3
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-08-25 15:19:41 -0700

    chore: Add Bun project files and dependencies

---
+++ software-development/web-development/backend-development/bun/quickstart/index.ts
@@ -0,0 +1,13 @@
+import figlet from "figlet";
+
+const server = Bun.serve({
+  port: 3000,
+  fetch(req) {
+    return new Response("Hello from my first Bun server");
+  },
+});
+
+const figOpts: figlet.Options = { horizontalLayout: "full"}
+
+console.log(figlet.textSync("New Bun Server", figOpts));
+console.log(`Listening on http://localhost:${server.port} ...`);
