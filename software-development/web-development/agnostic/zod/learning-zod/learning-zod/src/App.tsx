30b3565a01034a729736705c378f57e60377356e
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2024-09-20 14:27:43 -0700

    feat: Add zod library for schema validation
    Add the zod library as a dependency in the package.json file to enable schema validation in the project. Also, import and use the zod library in the App.tsx file to demonstrate how to define and validate object schemas using zod.

--- software-development/web-development/agnostic/zod/learning-zod/learning-zod/src/App.tsx
+++ software-development/web-development/agnostic/zod/learning-zod/learning-zod/src/App.tsx
@@ -1,7 +1,51 @@
 import "./App.css";
+import { z } from "zod";
+
+// Zod is a TypeScript-first schema declaration and validation library. It is designed to be as simple as possible, and to be as easy to use as possible.
+// We define a schema using the z object, and then we can use the parse method to validate the object against the schema.
+// A schema is just a fancy way of saying a set of rules that an object must follow in order to be considered valid.
+// A schema can also be used to generate a type for the object that it describes so we can type check our objects against the schema in the code for build time type checking.
+const userSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  email: z.string().email(),
+  hobbies: z.array(
+    z.object({
+      name: z.string(),
+      years: z.number(),
+      frequency: z.enum(["daily", "weekly", "monthly"]).optional(),
+    })
+  ),
+});
+
+// The infer keyword is used to get the type of a schema. This is useful when we want to use the schema to generate a type for an object
+// And get type static type checking and safety in our code.
+type User = z.infer<typeof userSchema>;
+
+// const user = {
+//   firstName: 1, // should be a string - Zod will validate this and throw an error (or success: false if using .safeParse)
+//   lastName: "Barcelos",
+// };
+
+const user2: User = {
+  firstName: "Tanner",
+  lastName: "Barcelos",
+  email: "test@gmail.com",
+  hobbies: [
+    {
+      name: "Programming",
+      years: 5,
+      frequency: "daily",
+    },
+  ],
+};
+
+// console.log(userSchema.parse(user)); // throws an error
+// console.log(userSchema.safeParse(user)); // { success: false, error: ZodError }
+console.log(userSchema.safeParse(user2)); // { success: true, data: { firstName: 'Tanner', lastName: 'Barcelos' } }
 
 function App() {
-  return <h1>Hello</h1>;
+  return <h1>Learning Zod!</h1>;
 }
 
 export default App;
